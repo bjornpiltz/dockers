c484b36bba7d32b18ca9363310957ea6acc138c9
Björn Piltz <bjornpiltz@gmail.com>
2016-12-13 17:41:26 +0100

    FIxed typo.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker build -t openmvg-deps openmvg-deps
-docker build --build-arg NUM_CORES=4 -t open-mvg-thirdparty open-mvg-thirdparty
+docker build --build-arg NUM_CORES=4 -t openmvg-thirdparty openmvg-thirdparty
 docker build -t openmvg-clone openmvg-clone
 docker build --build-arg NUM_CORES=2 -t openmvg-build openmvg-build
 
